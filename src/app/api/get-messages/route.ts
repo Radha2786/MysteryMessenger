0d7a4fc02fdce2bacad8cacd339885ff88cc121a
Radha Sharma <radhasharma2786@gmail.com>
2024-09-20 14:43:51 +0530

    fixes in get-msgs-api

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -7,9 +7,12 @@ import mongoose from "mongoose";
 
 export async function GET(request:Request){
  await dbConnect();
-//  console.log("inside get messages", request);
+ console.log("inside get messages", request);
  const session = await getServerSession(authOptions);
- const _user : User = session?.user;
+ const _user: User = session?.user;
+
+//  console.log("session inside get messages", session)
+//  console.log("_user inside get messages", _user)
 
  if(!session || !_user){
      return Response.json({success:false, message:"Unauthorized"}, {status:401});
@@ -19,17 +22,22 @@ export async function GET(request:Request){
 //  const userId = user._id; // ye id actually m ek string hai which will create issue in aggregation pipeline so we need to convert it to mongoose ka object id 
 const userId = new mongoose.Types.ObjectId(_user._id);
 
+// console.log("userId inside get-messages is", userId);
+
 try{
-    // console.log("userId is", userId);
     const user = await UserModel.aggregate([
         { $match : { _id : userId}},
-        {$unwind : '$messages'},
+        {$unwind : {path: '$messages', preserveNullAndEmptyArrays: true}},
         {$sort: {'messages.createdAt': -1}},
         {$group:{_id: '$_id', messages:{$push: '$messages'}}},
     ]).exec();
 
-    // console.log("user is", user);
-    if(!user || user.length === 0){
+    // console.log("user inside getMessage is", user);
+    // console.log("user[0].messages inside getMessage is", user[0].messages);
+
+    if(user[0].messages.length==0){
+        return Response.json({success:true, message:"No messages found"}, {status:404});
+    }else if(!user || user.length === 0){
         return Response.json({success:false, message:"User not found"}, {status:404});
     }
 
